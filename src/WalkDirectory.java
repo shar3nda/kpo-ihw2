24d406eb676b0a8de665359dfdda5bb6c3bcd32a
shar3nda <vyavya2003@yandex.ru>
2022-12-12 13:12:55 +0300

    implement directory walk

---
+++ src/WalkDirectory.java
@@ -0,0 +1,26 @@
+import java.io.File;
+import java.util.function.Consumer;
+
+public final class WalkDirectory {
+
+    /**
+     * Функция проходится по всем файлам в текущей папке и во всех вложенных в нее, выполняя операцию {@code fileConsumer} над каждым файлом.
+     *
+     * @param dir          Путь к корневой папке
+     * @param fileConsumer Метод, производящий нужное действие над файлом
+     */
+    public static void walk(File dir, Consumer<File> fileConsumer) {
+        if (!dir.isDirectory()) {
+            fileConsumer.accept(dir);
+            return;
+        }
+        File[] files = dir.listFiles();
+        if (files == null) {
+            System.out.println("I/O error occured");
+            return;
+        }
+        for (File file1 : files) {
+            walk(file1, fileConsumer);
+        }
+    }
+}
