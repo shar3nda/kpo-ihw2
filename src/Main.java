8b0587fca075cb56b89c0aea21a66ee43182b646
shar3nda <vyavya2003@yandex.ru>
2022-12-12 13:13:05 +0300

    implement directory walk

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
+import java.io.File;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        WalkDirectory.walk(new File("files"), f -> System.out.println(f.getAbsolutePath()));
     }
 }
